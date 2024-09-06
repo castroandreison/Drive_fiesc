573a9f6b7e45d90e1503b972e2651bdf91a3ab64
castroandreison <60223658+castroandreison@users.noreply.github.com>
2024-09-06 16:09:57 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,17 @@
 import streamlit as st
-import gspread
-import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
-# Conectar ao Google Sheets sem autenticação (para planilhas públicas)
-gc = gspread.Client(auth=None)
-gc.session = gspread.httpsession.HTTPSession()
+# Criação de um objeto de conexão.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Abrir a planilha pelo URL público
-spreadsheet = gc.open_by_url("https://docs.google.com/spreadsheets/d/1t2Ly9Qga99EpjUryL9pAMJU2DZxent426gVy-y0gNis/edit?usp=sharing")
-worksheet = spreadsheet.get_worksheet(0)  # Seleciona a primeira aba
+# Ler os dados da planilha.
+df = conn.read()
 
-# Ler os dados da planilha
-data = worksheet.get_all_records()
-df = pd.DataFrame(data)
-
-# Exibir os dados no Streamlit
+# Exibir os dados no Streamlit.
 st.write("Dados da Planilha:")
 st.dataframe(df)
 
-# Exibir os dados linha por linha
+# Exibir os dados linha por linha.
 st.write("Visualizando os dados linha a linha:")
-for index, row in df.iterrows():
-    st.write(f"{row['Coluna1']} tem um {row['Coluna2']}")
+for row in df.itertuples():
+    st.write(f"{row[1]} tem um {row[2]}")
