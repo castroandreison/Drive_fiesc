4ac2f779687c93dbd7a5f9bf8275f91b18af668e
castroandreison <60223658+castroandreison@users.noreply.github.com>
2024-09-06 15:35:53 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,11 @@
+# example/st_app.py
+
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Conexão com o Google Sheets
+url = "https://docs.google.com/spreadsheets/d/1t2Ly9Qga99EpjUryL9pAMJU2DZxent426gVy-y0gNis/edit?usp=sharing"
+
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Ler os dados da planilha
-df = conn.read()
-
-# Exibir os dados no Streamlit
-st.write("Dados da Planilha:")
-st.dataframe(df)
-
-# Exibir os dados linha por linha
-st.write("Visualizando os dados linha a linha:")
-for row in df.itertuples():
-    st.write(f"{row[1]} tem um {row[2]}")
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
