11f529309210fa4b3b281c2f2fcdd2566fbff695
castroandreison <60223658+castroandreison@users.noreply.github.com>
2024-09-09 14:56:06 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,19 @@
+# streamlit_app.py
+
 import streamlit as st
-import pygsheets
-import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
-# Autenticar com o pygsheets usando o caminho seguro das credenciais
-credentials_path = st.secrets["gsheets"]["credentials_path"]
-gc = pygsheets.authorize(service_file=credentials_path)
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# URL do Google Sheets
-sheet_url  = "https://docs.google.com/spreadsheets/d/1t2Ly9Qga99EpjUryL9pAMJU2DZxent426gVy-y0gNis/"
-spreadsheet = gc.open_by_url(sheet_url)
+df = conn.read()
 
-# Selecionar a primeira aba da planilha (ou escolha pelo nome da aba)
-worksheet = spreadsheet[0]
-
-# Ler os dados da planilha e selecionar colunas específicas (colunas 0 e 5)
-df = worksheet.get_as_df()  # Ou usar parameters como start, end para delimitar as colunas
-
-# Selecionar colunas 0 e 5
-df_selected = df.iloc[:, [0,1,2,3,4, 5]]
-
-# Exibir os dados no Streamlit
-st.dataframe(df_selected)
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
+    df = conn.read(
+    worksheet="0",
+    ttl="10m",
+    usecols=[0, 1],
+    nrows=3,
+)
