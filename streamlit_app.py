1772ab43e50c126160b38271bcecd4ef98b3d9a4
castroandreison <60223658+castroandreison@users.noreply.github.com>
2024-10-16 16:39:49 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,18 @@
 import streamlit as st
-from st_files_connection import FilesConnection
 
-# Tentar criar o objeto de conexão e ler o arquivo CSV
-try:
-    # Criar o objeto de conexão e recuperar o conteúdo do arquivo
-    conn = st.connection('gcs', type=FilesConnection)
-    df = conn.read("streamlit-bucket/fiesc.csv", input_format="csv", ttl=600)
-    
-    # Verificar se as colunas necessárias estão presentes
-    if 'n' in df.columns and 'amplitude' in df.columns:
-        # Se as colunas estiverem presentes, imprimir os resultados
-        for row in df.itertuples():
-            st.write(f"{row.n} has a :{row.amplitude}:")
-    else:
-        # Se as colunas não forem encontradas, exibir uma mensagem de erro
-        st.error("As colunas 'n' e 'amplitude' não foram encontradas no arquivo CSV.")
+from streamlit_gsheets import GSheetsConnection
 
-# Tratar possíveis exceções de leitura de arquivos ou problemas de conexão
-except Exception as e:
-    st.error(f"Ocorreu um erro ao ler o arquivo CSV: {e}")
+# Create GSheets connection
+conn = st.connection("0", type=GSheetsConnection)
+
+# Read Google WorkSheet as DataFrame
+df = conn.read(
+    worksheet="Example 1",
+    usecols=[
+        0,
+        1,
+    ],  # specify columns which you want to get, comment this out to get all columns
+)
+
+# Display our Spreadsheet as st.dataframe
+st.dataframe(df)
